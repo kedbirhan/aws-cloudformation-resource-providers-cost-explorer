ef2318a31f7f6c7c4bb92cd4e91f229c07e894c2
kedbirhan <birhan365@gmail.com>
2023-02-02 20:14:41 -0500

    Update ListHandler.java

--- anomalymonitor/src/main/java/software/amazon/ce/anomalymonitor/ListHandler.java
+++ anomalymonitor/src/main/java/software/amazon/ce/anomalymonitor/ListHandler.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+// test
 public class ListHandler extends AnomalyMonitorBaseHandler {
 
     public ListHandler() {
